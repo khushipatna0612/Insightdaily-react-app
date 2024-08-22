47a9e686807f4fbd71fd69f630bb4e6cd2c914ce
Khushi <142216330+khushipatna0612@users.noreply.github.com>
2024-08-22 13:47:27 +0530

    Upload InsightDaily news app project

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const NewsItem = (props) =>{
+    let {title, description, imageUrl, newsUrl, author, date, source} = props;
+    return (
+      <div className="my-3">
+        <div className="card">
+        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>
+        {source}</span>
+          <img src={!imageUrl?"https://static.toiimg.com/thumb/msid-112079450,width-1070,height-580,imgsize-13650,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg" : imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-muted">By {!author?"Unknown": author} on {new Date(date).toGMTString()}</small></p>
+            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default NewsItem
